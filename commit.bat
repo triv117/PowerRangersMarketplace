5ba69db26ec711e3053d532429a6284ddbb5ce8b
Tommy Rivard <82054791+triv117@users.noreply.github.com>
2022-11-10 10:29:45 -0500

    Add files via upload

---
+++ commit.bat
@@ -0,0 +1,4 @@
+git add *
+git config user.name "Michel Paquette
+git config user.email "paquettm@vaniercollege.qc.ca"
+git commit %*
---
+++ site.sql
@@ -0,0 +1,192 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 27, 2022 at 06:12 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `site`
+--
+CREATE DATABASE IF NOT EXISTS `site` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `site`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `merchant`
+--
+
+CREATE TABLE `merchant` (
+  `merchant_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `merchant_name` varchar(50) NOT NULL,
+  `merchant_email` varchar(100) NOT NULL,
+  `merchant_phone` int(15) NOT NULL,
+  `merchant_address` text NOT NULL,
+  `merchant_city` varchar(50) NOT NULL,
+  `merchant_country` varchar(50) NOT NULL,
+  `merchant_region` varchar(50) NOT NULL,
+  `merchant_zip` varchar(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL,
+  `mechant_id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_description` text NOT NULL,
+  `product_units` int(11) NOT NULL,
+  `product_price` decimal(10,0) NOT NULL,
+  `product_manufacturer` varchar(50) NOT NULL,
+  `product_rating` decimal(10,0) NOT NULL,
+  `product_category` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shipping`
+--
+
+CREATE TABLE `shipping` (
+  `shipping_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `merchant_id` int(11) NOT NULL,
+  `transaction_id` int(11) NOT NULL,
+  `shipping_conf_date` date NOT NULL,
+  `shipping_arrival_date` date NOT NULL,
+  `shipping_status` enum('confirmed','shipping to warehouse','held','shipping to customer','complete','cancelled') NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `shipping_product_units` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_username` varchar(50) NOT NULL,
+  `user_fname` varchar(50) NOT NULL,
+  `user_lname` varchar(50) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `user_phone` varchar(15) NOT NULL,
+  `user_address` text NOT NULL,
+  `user_country` varchar(50) NOT NULL,
+  `user_region` varchar(50) NOT NULL,
+  `user_zip` varchar(50) NOT NULL,
+  `user_password_hash` varchar(256) NOT NULL,
+  `user_secret` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `merchant`
+--
+ALTER TABLE `merchant`
+  ADD PRIMARY KEY (`merchant_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `merchant_name` (`merchant_name`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `product_name` (`product_name`),
+  ADD KEY `merchant_id` (`mechant_id`);
+
+--
+-- Indexes for table `shipping`
+--
+ALTER TABLE `shipping`
+  ADD PRIMARY KEY (`shipping_id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `merchant_id2` (`merchant_id`),
+  ADD KEY `user_id2` (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `user_username` (`user_username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `merchant`
+--
+ALTER TABLE `merchant`
+  MODIFY `merchant_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `shipping`
+--
+ALTER TABLE `shipping`
+  MODIFY `shipping_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `merchant`
+--
+ALTER TABLE `merchant`
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `merchant_id` FOREIGN KEY (`mechant_id`) REFERENCES `merchant` (`merchant_id`);
+
+--
+-- Constraints for table `shipping`
+--
+ALTER TABLE `shipping`
+  ADD CONSTRAINT `merchant_id2` FOREIGN KEY (`merchant_id`) REFERENCES `merchant` (`merchant_id`),
+  ADD CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`),
+  ADD CONSTRAINT `user_id2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
