cde329172adc6d2ba6a896b58def6731bc040fd6
triv117 <tomriv117@outlook.com>
2022-12-08 15:54:48 -0500

    Wishlist Fix

--- app/controllers/Wishlist.php
+++ app/controllers/Wishlist.php
@@ -4,25 +4,24 @@ namespace app\controllers;
 class Cart extends \app\core\Controller{
 
 	public function addToWishlist($product_id){
+		$product = new app\models\product();
+		$product = $product->get($product_id);
+
 		if(isset($_POST['action'])){
 			$wishlist = new app\models\Wishlist();
 			$wishlist = $wishlist->getByUser($_SESSION['user_id']);
-
-			$product = new app\models\product();
-			$product = $product->get($product_id);
-
 			$wishlist->product_id = $product_id;
 			$wishlist->insert();
-			header('location:/Product/index',['product'->$product]);
+			header('location:/Product/index?message=Item added to wishlist', $product);
 		}else{
-			$this->view('Product/index',['product'->$product]);
+			$this->view('Product/index', $product);
 		}
 	}
 
 	public function index(){
 		$wishlist = new app\models\Wishlist();
 		$wishlist = $wishlist->getByUser($_SESSION['user_id']);
-		$this->view('Wishlist/index',['wishlist'->$wishlist]);
+		$this->view('Wishlist/index', $wishlist);
 	}
 
 	public function deleteFromWishlist($product_id){
@@ -32,6 +31,6 @@ class Cart extends \app\core\Controller{
 		$wishlist = new app\models\Wishlist();
 		$wishlistEntry = $wishlist->get($_SESSION['user_id'], $product_id);
 		$wishlistEntry->delete();
-		$this->view('Wishlist/index',['wishlist'->$wishlist]);
+		$this->view('Wishlist/index', $wishlist);
 	}
 }
